de62409386e7f20d2d8acbea4b20261f5957343c
semenInRussia <semenprofis999@gmail.com>
2020-05-31 18:33:18 +0300

    add skell for tests

---
+++ tests/test.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestDecode(unittest.TestCase):
+    def tearDown(self) -> None:
+        pass
+
+
+class TestParse(unittest.TestCase):
+    def tearDown(self) -> None:
+        pass
+
+
+class TestWrite(unittest.TestCase):
+    def tearDown(self) -> None:
+        pass
+
+
+unittest.main()
