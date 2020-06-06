22bab428fc8315f8e25898ee4b7208348fd538de
semenInRussia <semenprofis999@gmail.com>
2020-06-06 22:55:31 +0300

    add tests for parse one tests, rename parser on syntax, add __init__.py in packages

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,14 @@
+import os
+import sys
+
+
 import unittest
+
+BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+yaml_file_path: str = os.path.join(BASE_DIR, 'tests', 'test_one_step.yml')
+
+from syntax.functions import parse_yaml_file
 
 
 class TestDecode(unittest.TestCase):
@@ -10,10 +20,15 @@ class TestParse(unittest.TestCase):
     def tearDown(self) -> None:
         pass
 
+    def test_one_step(self):
+        data = parse_yaml_file(yaml_file_path)
+        self.assertIsInstance(data, dict)
+
 
 class TestWrite(unittest.TestCase):
     def tearDown(self) -> None:
         pass
 
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
