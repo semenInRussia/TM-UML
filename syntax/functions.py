407c66a04c46e049f4a31cd03e80486d37c7151c
semenInRussia <semenprofis999@gmail.com>
2020-06-08 21:44:07 +0300

    complete tests

--- syntax/functions.py
+++ syntax/functions.py
@@ -1,8 +1,14 @@
 import os
+import yaml
 
+# disable errors
+yaml.warnings({'YAMLLoadWarning': False})
 
+# main function
 def parse_yaml_file(filename: str):
     """Parse data from .yml files"""
     if not os.path.exists(filename):
         raise FileNotFoundError
-    return {}
+
+    with open(filename) as f:
+        return yaml.load(f)
