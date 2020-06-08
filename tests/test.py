286dd88faac64078651af8b6d85eda89f6507505
semenInRussia <semenprofis999@gmail.com>
2020-06-08 21:31:35 +0300

    add test valid content in tests

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 
@@ -6,7 +7,6 @@ import unittest
 
 BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-yaml_file_path: str = os.path.join(BASE_DIR, 'tests', 'test_one_step.yml')
 
 from syntax.functions import parse_yaml_file
 
@@ -17,16 +17,27 @@ class TestDecode(unittest.TestCase):
 
 
 class TestParse(unittest.TestCase):
+    yaml_file_path: str = os.path.join(BASE_DIR, 'tests', 'test_one_step.yml')
+    json_file_path: str = os.path.join(BASE_DIR, 'tests', 'test_one_step.json')
+
     def tearDown(self) -> None:
         pass
 
     def test_one_step(self):
-        data = parse_yaml_file(yaml_file_path)
+        data = parse_yaml_file(self.yaml_file_path)
         self.assertIsInstance(data, dict)
 
     def test_not_file_error(self):
         with self.assertRaises(FileNotFoundError):
             parse_yaml_file('jjjjjjjhghbfgddfvjjj')
+
+    def test_valid_content(self):
+        with open(self.json_file_path) as f:
+            valid_data = json.load(f)
+
+        data = parse_yaml_file(self.yaml_file_path)
+
+        self.assertEqual(valid_data, data)
 
 
 class TestWrite(unittest.TestCase):
